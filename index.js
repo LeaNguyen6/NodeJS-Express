1761a08bda4b6e3b83c8148672d909dfffaea331
Phuong <julia.nguyen0616@gmail.com>
2020-05-06 17:31:09 +0700

    5.nodemon + 6- Database (lowdb)

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+//6- Database (lowdb)
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+db.defaults({ users: [] })
+  .write()
 //2 - Template engines
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -9,20 +17,14 @@ app.set('views', './views')
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-
-let users=[
-    {id:1,name:'avd'},
-    {id:2,name:'Khuon'},
-    {id:3,name:'Phuong'}
-]
 app.get('/', (req, res) => res.render('index', { title: 'Hey', message: 'Hello there!' }))
 app.get('/users',(req,res)=>{res.render('users/index',{
-    users
+    users:db.get('users').value()
 })})
 //3 - Query parameters
 app.get('/users/search',(req,res)=>{
     let q=req.query.q;
-    let findUser=users.filter(x=>x.name.toLowerCase().indexOf(q.toLowerCase()) !== -1);
+    let findUser=db.get('users').value().filter(x=>x.name.toLowerCase().indexOf(q.toLowerCase()) !== -1);
     res.render('users/index',{
         users:findUser,
         q
@@ -33,7 +35,7 @@ app.get('/users/create',(req,res)=>{
     res.render('users/create')
 })
 app.post('/users/create',(req,res)=>{
-    users.push(req.body)
+    db.get('users').push(req.body).write()
     res.redirect('/users')
 })
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
