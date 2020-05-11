c69e924ff5012b343093c74f604b12228d88bbc6
Phuong <julia.nguyen0616@gmail.com>
2020-05-11 17:25:27 +0700

    12 - Server-side validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -25,6 +25,16 @@ module.exports.viewUser=(req, res) => {
     })
 }
 module.exports.create=(req, res) => {
+    let errs=[]
+    if (!req.body.name){errs.push('Name is required')}
+    if (!req.body.phone){errs.push('Phone is required')}
+    if (errs.length) {
+        res.render('users/create',{
+            errs,
+            value:req.body
+        })
+        return;
+    }
     req.body.id = shortid.generate();
     db.get('users').push(req.body).write()
     res.redirect('/users')
